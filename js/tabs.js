7709e2de1fc2fd398408f780c5de57358b05f1cf
Joshua Holmes <hello@joshuaholmes.id.au>
2014-08-15 22:16:31 +1000

    Addition of tabs
    Home page tabs added, Preorder form added, Business Enquiry form added,
    fixed up HTML spacing.

---
+++ js/tabs.js
@@ -0,0 +1,20 @@
+$('#home a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+
+$('#consumer a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+
+$('#preorder a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+
+$('#businessEnq a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+
